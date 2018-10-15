cf9cdcc24989f206360f9273876cd76ba6b574ab
Tanner Cranor <tbudc@yahoo.com>
2018-10-15 13:33:48 -0600

    Stub Views
    description
    - basic frames for our Views

---
+++ components/toolbar.js
@@ -0,0 +1,10 @@
+import { React, Component } from "react";
+import {Text} from "react-native";
+
+export default class Toolbar extends Component {
+  render() {
+    return (
+        <Text>This is obviously not ready</Text>
+    );
+  }
+}
