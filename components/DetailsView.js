53a3174ae43ce57a45160c9492140c30569601f5
Tanner Cranor <tbudc@yahoo.com>
2018-11-02 15:08:31 -0600

    Changed detailsview to represent bool reserved

--- components/DetailsView.js
+++ components/DetailsView.js
@@ -5,13 +5,14 @@ import { Card } from 'react-native-elements';
 export default class DetailsView extends Component {
   constructor() {
     super()
-    this.state = {
-      reserved: false
-    }
 
     this.reserveClicked = this.reserveClicked.bind(this);
     this.reserveConfirmed = this.reserveConfirmed.bind(this);
-    this.reserveCancelled = this.reserveCancelled.bind(this);
+
+    this.unreserveClicked = this.unreserveClicked.bind(this);
+    this.unreserveConfirmed = this.unreserveConfirmed.bind(this);
+    
+    this.actionCancelled = this.actionCancelled.bind(this);
 
   }
 
@@ -22,7 +23,7 @@ export default class DetailsView extends Component {
       'Make Reservation?',
       [
         {text: 'Make Reservation', onPress: this.reserveConfirmed},
-        {text: 'Cancel', onPress: this.reserveCancelled, style: 'cancel'}
+        {text: 'Cancel', onPress: this.actionCancelled, style: 'cancel'}
       ]
     );
   }
@@ -51,8 +52,42 @@ export default class DetailsView extends Component {
       
   }
 
-  reserveCancelled() {
-    
+  unreserveClicked() {
+    Alert.alert(
+      'Confirm Action',
+      'Undo this reservation?',
+      [
+        {text: 'Undo Reservation', onPress: this.unreserveConfirmed},
+        {text: 'Cancel', onPress: this.actionCancelled, style: 'cancel'}
+      ]
+    );
+  }
+
+  unreserveConfirmed() {
+    let listing = this.props.navigation.state.params.listing;
+    fetch('http://3.16.22.45:3000/api/reservation',{
+      method: "POST",
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "renter_id": listing.owner_id,
+        "spot_id": listing.id,
+        "start_datetime": 5555590000,
+        "end_datetime": 5555990419,
+        "is_cancelled": 0
+      })
+    })
+      .then((response) => response.json())
+      .then((dataResponse) => {
+        console.log(dataResponse)
+      })
+      
+  }
+
+  actionCancelled() {
+    console.log('cancelled choice')
   }
   
   render() {
@@ -65,7 +100,7 @@ export default class DetailsView extends Component {
         <Text style={{ marginBottom: 10 }}>{listing.street_address}</Text>
         <Text style={{ marginBottom: 10 }}>{listing.cost_per_hour}$ per hour</Text>
         <Text style={{ marginBottom: 10 }}>{listing.description}</Text>
-        <Button onPress={this.reserveClicked} title="Reserve Listing"/>
+        {listing.reserved ? (<Button onPress={this.unreserveClicked} title='Cancel Reservation'/>) : (<Button onPress={this.reserveClicked} title='Reserve Listing'/>)}
       </View>
     );
   }
