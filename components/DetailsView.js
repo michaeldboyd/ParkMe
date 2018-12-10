b5739f9798f24ba7b2e9fc19260a83421e1a6faf
Michael Boyd <michael@Andrews-iPhone-010034227208.app.byu.edu>
2018-12-10 13:04:58 -0700

    added new details view. STILL UNTESTED'
    Signed-off-by: Michael Boyd <michael@Andrews-iPhone-010034227208.app.byu.edu>

--- components/DetailsView.js
+++ components/DetailsView.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Button, Image, Alert } from "react-native";
+import { ImageBackground, StyleSheet, Text, View, Button, Image, Alert, ScrollView,  TouchableOpacity } from "react-native";
 import { Card } from 'react-native-elements';
 import FooterTabs from "./Footer";
-
+import { Dimensions } from 'react-native'
+// import PropTypes from 'prop-types'
 
 export default class DetailsView extends Component {
   constructor() {
@@ -96,29 +97,121 @@ export default class DetailsView extends Component {
   actionCancelled() {
     console.log('cancelled choice')
   }
+// -------------------------------------
 
-  render() {
-    let listing = this.props.navigation.state.params.listing;
-    return (
-      <View style={styles.container}>
-        <Image style={{ marginBottom: 20, width: 200, height: 200 }} source={{ uri: listing.parking_image_path === null ? 'https://www.bristolgate.com/wp-content/uploads/2018/09/orionthemes-placeholder-image.png' : listing.parking_image_path }}></Image>
-        <Text style={{ marginBottom: 10 }}>{listing.first_name + " " + listing.last_name}</Text>
-        <Text style={{ marginBottom: 10 }}>{listing.street_address}</Text>
-        <Text style={{ marginBottom: 10 }}>${listing.cost_per_hour} per hour</Text>
-        {/* <Text style={{ marginBottom: 10 }}>{listing.description}</Text> */}
-        {this.state.reserved === 1 ? (<Button onPress={this.unreserveClicked} title='Cancel Reservation' />) : (<Button onPress={this.reserveClicked} title='Reserve Listing' />)}
+// renderDetail = () => {
+//   return (
+//     <View>
+//       <Text style={styles.detailText}>For Sale Property Details</Text>
+//       <Text style={styles.subDetailText}>{this.props.detail}</Text>
+//     </View>
+//   )
+// }
 
-        <View style={styles.footerContainer}>
-          <FooterTabs active={4} getListings={this.props.navigation.state.params.getListings} navigation={this.props.navigation} />
-        </View>
-      </View>
-    );
-  }
+renderDescription = () => {
+  return (
+    <View>
+      <Text style={styles.descriptionText}>{listing.first_name + " " + listing.last_name}</Text>
+      <Text sstyle={styles.descriptionText}>{listing.street_address}</Text>         
+      <Text style={styles.descriptionText}>${listing.cost_per_hour} per hour</Text>
+      {this.state.reserved === 1 ? (<Button onPress={this.unreserveClicked} title='Cancel Reservation' />) : (<Button onPress={this.reserveClicked} title='Reserve Listing' />)}
+    </View>
+  )
 }
+
+// renderNavigator = () => {
+//   return (
+//     <View
+//       style={{
+//         flexDirection: 'row',
+//       }}
+//     >
+//       <TouchableOpacity style={[styles.navigatorButton, { flex: 2 }]}>
+//         <Text style={styles.navigatorText}>DIRECTIONS</Text>
+//       </TouchableOpacity>
+//       <TouchableOpacity style={[styles.navigatorButton, { flex: 2 }]}>
+//         <Text style={styles.navigatorText}>STREET VIEW</Text>
+//       </TouchableOpacity>
+//       <TouchableOpacity style={[styles.navigatorButton, { flex: 1 }]}>
+//         <Text style={styles.navigatorText}>MAP</Text>
+//       </TouchableOpacity>
+//     </View>
+//   )
+// }
+
+renderContactHeader = () => {
+  return (
+    <View style={styles.headerContainer}>
+      <View style={styles.coverContainer}>
+        <ImageBackground
+          source={{
+            uri: listing.parking_image_path === null ? 'https://www.bristolgate.com/wp-content/uploads/2018/09/orionthemes-placeholder-image.png' : listing.parking_image_path
+          }}
+          style={styles.coverImage}
+        >
+        </ImageBackground>
+      </View>
+    </View>
+  )
+}
+
+render() {
+  return (
+    <View style={styles.mainviewStyle}>
+      <ScrollView style={styles.scroll}>
+        <View style={[styles.container, this.props.containerStyle]}>
+          <View style={styles.cardContainer}>
+            {this.renderContactHeader()}
+          </View>
+        </View>
+        <View style={styles.productRow}>{this.renderDescription()}</View>
+      </ScrollView>
+      <View style={styles.footerContainer}>
+        <FooterTabs active={4} getListings={this.props.navigation.state.params.getListings} navigation={this.props.navigation} />
+      </View>
+    </View>
+  )
+}
+}
+//        <View style={styles.productRow}>{this.renderNavigator()}</View>
+// <View style={styles.productRow}>{this.renderDetail()}</View>
+// 
+
+/* 
+<View style={styles.footer}>
+        <TouchableOpacity style={styles.buttonFooter}>
+          <Text style={styles.textFooter}>CALL</Text>
+        </TouchableOpacity>
+        <View style={styles.borderCenter} />
+        <TouchableOpacity style={styles.buttonFooter}>
+          <Text style={styles.textFooter}>EMAIL</Text>
+        </TouchableOpacity>
+      </View>
+      */
+// ----------------------------------
+//   render() {
+//     let listing = this.props.navigation.state.params.listing;
+//     return (
+//       <View style={styles.container}>
+//         <Image style={{ marginBottom: 20, width: Dimensions.get('window').width}} source={{ uri: listing.parking_image_path === null ? 'https://www.bristolgate.com/wp-content/uploads/2018/09/orionthemes-placeholder-image.png' : listing.parking_image_path }}></Image>
+//         <Text style={{ marginBottom: 10 }}>{listing.first_name + " " + listing.last_name}</Text>
+//         <Text style={{ marginBottom: 10 }}>{listing.street_address}</Text>
+//         <Text style={{ marginBottom: 10 }}>${listing.cost_per_hour} per hour</Text>
+//         {/* <Text style={{ marginBottom: 10 }}>{listing.description}</Text> */}
+//         {this.state.reserved === 1 ? (<Button onPress={this.unreserveClicked} title='Cancel Reservation' />) : (<Button onPress={this.reserveClicked} title='Reserve Listing' />)}
+
+//         <View style={styles.footerContainer}>
+//           <FooterTabs active={4} getListings={this.props.navigation.state.params.getListings} navigation={this.props.navigation} />
+//         </View>
+//       </View>
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
@@ -136,5 +229,108 @@ const styles = StyleSheet.create({
   Image: {
     padding: "20%",
     margin: "20%"
-  }
+  },
+  cardContainer: {
+    flex: 1,
+  },
+  coverContainer: {
+    position: 'relative',
+  },
+  coverImage: {
+    height: Dimensions.get('window').width * (3 / 4),
+    width: Dimensions.get('window').width,
+  },
+  headerContainer: {
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+  },
+  scroll: {
+    backgroundColor: '#FFF',
+    flex: 1,
+    marginBottom: 55,
+  },
+  productRow: {
+    margin: 25,
+  },
+  mainviewStyle: {
+    flex: 1,
+    flexGrow: 1,
+    flexDirection: 'column',
+  },
+  coverMetaContainer: {
+    alignItems: 'flex-end',
+    flex: 1,
+    justifyContent: 'flex-end',
+    // marginBottom: 15,
+    // marginRight: 15,
+  },
+  footer: {
+    position: 'absolute',
+    flex: 0.1,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: '#F64A25',
+    flexDirection: 'row',
+    height: 65,
+    alignItems: 'center',
+  },
+  buttonFooter: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  navigatorButton: {
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  navigatorText: {
+    color: 'green',
+    fontWeight: 'bold',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+
+    fontSize: 16,
+  },
+  borderCenter: {
+    height: 55,
+    borderWidth: 0.5,
+    borderColor: '#FFA890',
+  },
+  textFooter: {
+    color: 'white',
+    fontWeight: 'bold',
+    alignItems: 'center',
+    fontSize: 18,
+  },
+  priceText: {
+    marginBottom: 5,
+    letterSpacing: 1,
+
+    color: 'black',
+    fontSize: 36,
+    fontWeight: '400',
+  },
+  detailText: {
+    marginBottom: 4,
+    color: 'black',
+    fontSize: 22,
+    fontWeight: '600',
+    letterSpacing: 0.5,
+  },
+  subDetailText: {
+    color: 'black',
+    fontSize: 16,
+    fontWeight: '100',
+    lineHeight: 28,
+    letterSpacing: 0.5,
+  },
+  descriptionText: {
+    marginBottom: 4,
+    color: 'gray',
+    fontSize: 16,
+    fontWeight: '400',
+    letterSpacing: 1,
+  },
 });
